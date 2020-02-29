18101dd02ab6d403c88feba3b64441a0c4259025
Paula Jazmin Rozo Coy <pjrozoc@gmail.com>
2020-02-29 19:22:53 +0100

    Revisar

---
+++ Clase_4.R
@@ -0,0 +1,12 @@
+# Actualizar R
+
+if(!require(installr)) {
+    install.packages("installr"); require(installr)} #load / install+load installr
+
+# using the package:
+updateR()
+
+installed.packages()
+library(rmarkdown)
+
+
